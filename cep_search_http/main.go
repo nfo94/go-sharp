73ddb3105a5c1db6b01fdff73ed76cc3afc2d7f1
Natália Oliveira <24434214+nfo94@users.noreply.github.com>
2026-02-25 08:26:12 +0000

    chore: add cep_search_http II

--- cep_search_http/main.go
+++ cep_search_http/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"io"
 	"net/http"
 	"net/url"
 )
@@ -42,13 +44,39 @@ func SearchCEPHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// TODO make GET request in
 	// https://viacep.com.br/ws/{cep}/json
-	fullUrl := url.Parse(VIACEP_URL + cepParam)
-	// http.Get()
+	fullUrl, err := url.Parse(VIACEP_URL + cepParam + "/json")
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	req, err := http.Get(fullUrl.String())
+	if err != nil {
+		w.WriteHeader(http.StatusBadGateway)
+		return
+	}
+	defer req.Body.Close()
+
+	res, err := io.ReadAll(req.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	var data CEP
+	err = json.Unmarshal(res, &data)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
 
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 
-	w.Write([]byte("Hello world"))
+	err = json.NewEncoder(w).Encode(data)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
 }
